4fbf019dca0d9d1e771d1801fdacfac040b92b79
info3170anantdeepdhingra <a_dhingra131254@fanshaweonline.ca>
2022-10-04 18:20:21 -0400

    Commit Extract

--- angular-info3170-store-bonsai-store-2022-09-27/src/app/app.component.ts
+++ angular-info3170-store-bonsai-store-2022-09-27/src/app/app.component.ts
@@ -1,9 +1,4 @@
-import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
-import { Product } from './product/product';
-import { MatDialog } from '@angular/material/dialog';
-import { ProductDialogComponent } from './product-dialog/product-dialog.component';
-import { ProductDialogResult } from './product-dialog/product-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -11,52 +6,5 @@ import { ProductDialogResult } from './product-dialog/product-dialog.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  constructor(private dialog: MatDialog) {}
-
-  products: Product[] = [
-    {
-      name: 'Product 1',
-      description: 'Description 1'
-    }
-  ];
-
-  wishList: Product[] = [];
-
-  shoppingCart: Product[] = [];
-
-  editProduct(list: string, product: Product): void {}
-
-  newProduct(): void {
-    const dialogRef = this.dialog.open(ProductDialogComponent, {
-      width: '300px',
-      data: {
-        product: {},
-      }
-    });
-    dialogRef
-      .afterClosed()
-      .subscribe((result: ProductDialogResult|undefined)=>{
-        if (!result) {
-          return;
-        }
-        this.products.push(result.product);
-      })
-  }
-
-  drop(event: CdkDragDrop<Product[]>): void {
-    if (event.previousContainer === event.container) {
-      return;
-    }
-    if (!event.container.data || !event.previousContainer.data) {
-      return;
-    }
-    transferArrayItem(
-      event.previousContainer.data,
-      event.container.data,
-      event.previousIndex,
-      event.currentIndex
-    );
-  }
-
+  title = 'bonsai-store';
 }
--- angular-info3170-store-bonsai-store-2022-09-27/src/app/app.module.ts
+++ angular-info3170-store-bonsai-store-2022-09-27/src/app/app.module.ts
@@ -1,37 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductDialogComponent } from './product-dialog/product-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductComponent,
-    ProductDialogComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule,
-    DragDropModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatInputModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
