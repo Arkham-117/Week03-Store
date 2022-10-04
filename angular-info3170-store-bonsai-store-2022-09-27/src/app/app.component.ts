db4b1fbec6ea5eed8449c2a283e2f24d5b07ab33
info3170anantdeepdhingra <a_dhingra131254@fanshaweonline.ca>
2022-10-04 13:04:11 -0400

    Module

--- angular-info3170-store-bonsai-store-2022-09-27/src/app/app.component.ts
+++ angular-info3170-store-bonsai-store-2022-09-27/src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { Product } from './product/product';
+import { MatDialog } from '@angular/material/dialog';
+import { ProductDialogComponent } from './product-dialog/product-dialog.component';
+import { ProductDialogResult } from './product-dialog/product-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bonsai-store';
+
+  constructor(private dialog: MatDialog) {}
+
+  products: Product[] = [
+    {
+      name: 'Product 1',
+      description: 'Description 1'
+    }
+  ];
+
+  wishList: Product[] = [];
+
+  shoppingCart: Product[] = [];
+
+  editProduct(list: string, product: Product): void {}
+
+  newProduct(): void {
+    const dialogRef = this.dialog.open(ProductDialogComponent, {
+      width: '300px',
+      data: {
+        product: {},
+      }
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: ProductDialogResult|undefined)=>{
+        if (!result) {
+          return;
+        }
+        this.products.push(result.product);
+      })
+  }
+
+  drop(event: CdkDragDrop<Product[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+
 }
